60bad372a2d629f2187375578fa78c0a0e4cb685
Colin <colin_yiyan@hotmail.com>
2024-04-05 23:51:27 +0800

    updated to live background with play/pause func

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const loadText = document.querySelector(".loading-text");
 const bg = document.querySelector(".bg");
+const bgVid = document.getElementById("background-video");
 
 let load = 0;
 
@@ -10,15 +11,26 @@ function blurring() {
 
   if (load > 99) {
     clearInterval(int);
+    loadText.remove();
   }
 
   console.log(load);
 
   loadText.innerText = `${load}%`;
   loadText.style.opacity = scale(load, 0, 100, 1, 0);
-  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+  bgVid.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
 }
 
 const scale = (num, in_min, in_max, out_min, out_max) => {
   return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
 };
+
+const button = document.getElementById("myButton");
+
+button.addEventListener("click", () => {
+  if (bgVid.paused) {
+    bgVid.play();
+  } else {
+    bgVid.pause();
+  }
+});
